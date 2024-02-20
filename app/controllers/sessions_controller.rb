2b4447f4ba231f91fabfb470897c86246eb72f04
Rob Vogel <rob.vogel@kellogg.northwestern.edu>
2024-02-20 15:27:30 +0000

    fixed loop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,28 +8,22 @@ class SessionsController < ApplicationController
     # 1. try to find the user by their unique identifier
 
     @user = User.find_by({"email" => params["email"]})
-
     # 2. if the user exists -> check if they know their password
-
     if @user != nil
     # 3. if they know their password -> login is successful
-
-    if @user["password"] == params["password"]
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
-    end
-
-    else
+       if @user["password"] == params["password"]
+       flash["notice"] = "Welcome."
+       redirect_to "/companies"
+       else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+       end
     
     # 4. if the user doesn't exist or they don't know their password -> login fails
+    else
     flash["notice"] = "Nope."
-    redirect_to "/companies"
-    end
-
-  else
-    flash["notice"] = "Nope."
-    redirect_to "/companies"
-  end  
+    redirect_to "/login"
+    end  
   
   end
 
